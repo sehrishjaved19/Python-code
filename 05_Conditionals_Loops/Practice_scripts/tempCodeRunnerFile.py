cee36c04dd4b92aee3dd0f7a72f7a29f9e866090
Sehrish <sehrishjaved119@gmail.com>
2026-04-12 17:41:42 +0500

    refactor: update folder names for better readability

---
+++ 05_Conditionals_Loops/Practice_scripts/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+post=input('Enter the post: ')
+
+if('sehrish' in post.lower()):#post.lower will convert the post into lower no matter what the input is
+    print('This post is talikng about Sehrish')
+else:
+    print('This post is not talikng about Sehrish')
